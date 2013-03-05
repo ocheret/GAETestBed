85944b8cf0648c013be0f540a7aef8bda54105b9
Chuck Ocheret <ocheret@panix.com>
2013-03-05 16:50:12 -0500

    Added Freemarker, SL44J, jQuery, Bootstrap, Underscore, JSON2. Log test added.

--- src/com/readyposition/gaetestbed/GAETestBedServlet.java
+++ src/com/readyposition/gaetestbed/GAETestBedServlet.java
@@ -3,10 +3,18 @@ package com.readyposition.gaetestbed;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SuppressWarnings("serial")
 public class GAETestBedServlet extends HttpServlet {
+    final static Logger logger = 
+            LoggerFactory.getLogger(GAETestBedServlet.class);
+
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
+            throws IOException
+    {
+        logger.info("GAETetBedServlet logger works!");
         resp.setContentType("text/plain");
         resp.getWriter().println("Hello, world");
     }
